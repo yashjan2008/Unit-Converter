87593444ba550c0e496ffa818174c32ac6e42319
Yash <yashr3712@gmail.com>
2026-07-26 17:24:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1 meter = 3.218 feet
+// 1 liter = 0.264 gallons
+// 1 kilogram = 2.204 pounds
+
+
+const btn = document.getElementById("convert-btn");
+let inputEl = document.getElementById("input-el");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const weightEl = document.getElementById("weight-el");
+const resetBtn = document.getElementById("reset-btn");
+
+inputEl.addEventListener("input", () => {
+    inputEl.size = Math.max(inputEl.value.length, 1);
+});
+
+resetBtn.addEventListener("click", function(){
+    inputEl.value = " "
+});
+
+btn.addEventListener("click", function(){
+    if (inputEl = Number(inputEl.value)){
+        let lengthResult = inputEl * 3.218;
+        lengthEl.textContent = lengthResult;
+        let volumeResult = inputEl * 0.264;
+        volumeEl.textContent = volumeResult;
+        let weightResult = inputEl * 2.204;
+        weightEl.textContent = weightResult;
+    }else{
+        lengthEl.textContent = "sorry!! Please enter numeric value";
+        volumeEl.textContent = "sorry!! Please enter numeric value";
+        weightEl.textContent = "sorry!! Please enter numeric value";
+        console.log("Sorry!!")
+    };
+    
+});
