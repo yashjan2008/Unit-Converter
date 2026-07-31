69144f6c041aaaf93fae7f0dc6aad7f1db6bd4c4
Yash <yashr3712@gmail.com>
2026-07-31 08:03:45 +0530

    apdating some logics and styls

--- script.js
+++ script.js
@@ -21,12 +21,14 @@ resetBtn.addEventListener("click", function(){
 btn.addEventListener("click", function(){
     let inputValue = Number(inputEl.value);
     if (!isNaN(inputValue)){
-        let lengthResult = inputEl.value * 3.218;
-        lengthEl.textContent = lengthResult;
-        let volumeResult = inputEl.value * 0.264;
-        volumeEl.textContent = volumeResult;
-        let weightResult = inputEl.value * 2.204;
-        weightEl.textContent = weightResult;
+        let meterResult = (inputEl.value * 3.218).toFixed(2);
+        let feetResult = (inputEl.value / 3.218).toFixed(2);
+        lengthEl.textContent = `${inputValue} meters = ${meterResult} feet | ${inputValue} feets = ${feetResult}`;
+        let literResult = (inputEl.value * 0.264).toFixed(2);
+        let gallonResult = (inputEl.value / 0.264).toFixed(2);
+        volumeEl.textContent = `${inputValue} liters = ${literResult} gallons | ${inputValue} gallons = ${gallonResult}`;
+        let weightResult = (inputEl.value * 2.204).toFixed(2);
+        weightEl.textContent = `${inputValue} kilograms = ${weightResult} pounds`;
     }else{
         alert("Please enter a numeric value!");
         inputEl.value = " ";
